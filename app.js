d4ff8b9251eaa80f749c21007ad904edaddee793
Marc <g40419@gmail.com>
2020-10-28 23:17:57 +0800

    1. install bcryptjs 2. install body-parser 3. add userController and signup page

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 const db = require('./models')
+const bodyParser = require('body-parser')
 
 app.engine('hbs', handlebars({
   defaultLayout: 'main',
   extname: '.hbs'
 }))
 app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController')
+const userController = require('../controllers/userController')
 
 module.exports = (app) => {
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
 
   app.get('/', (req, res) => res.redirect('/restaurants'))
   app.get('/restaurants', restController.getRestaurants)
