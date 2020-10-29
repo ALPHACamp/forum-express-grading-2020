cd36dae984945651dfa8a9dc1b8b16dc4cd0cccc
Marc <g40419@gmail.com>
2020-10-29 15:21:17 +0800

    add admin categories post

--- routes/index.js
+++ routes/index.js
@@ -42,4 +42,5 @@ module.exports = (app, passport) => {
   app.get('/admin/users', authenticatedAdmin, adminController.getUser)
   app.put('/admin/users/:id', authenticatedAdmin, adminController.putUser)
   app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
+  app.post('/admin/categories', authenticatedAdmin, categoryController.postCategory)
 }
