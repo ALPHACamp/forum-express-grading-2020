125a4cbd39d97890ff5733f20cf6e221041fbfb9
Harry <harry811016@gmail.com>
2020-11-02 18:52:27 +0800

    Install bcryptjsLink

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
 const db = require("./models");
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.engine("handlebars", handlebars()); // Handlebars 註冊樣板引擎
 app.set("view engine", "handlebars"); // 設定使用 Handlebars 做為樣板引擎
 // 設定 view engine 使用 handlebars
