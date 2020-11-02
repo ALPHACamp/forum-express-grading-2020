3259a9fbe12b5c5d1f60990e2fd5c56f0e876659
Harry <harry811016@gmail.com>
2020-11-02 18:55:00 +0800

    Install express-session connect-flash

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const db = require("./models");
 const app = express();
 const port = 3000;
+const flash = require("connect-flash");
+const session = require("express-session");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.engine("handlebars", handlebars()); // Handlebars 註冊樣板引擎
@@ -11,6 +13,14 @@ app.set("view engine", "handlebars"); // 設定使用 Handlebars 做為樣板引
 // 設定 view engine 使用 handlebars
 app.engine("handlebars", handlebars());
 app.set("view engine", "handlebars");
+app.use(session({ secret: "secret", resave: false, saveUninitialized: false }));
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash("success_messages");
+  res.locals.error_messages = req.flash("error_messages");
+  next();
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
