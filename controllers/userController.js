f164bf59d3c3e9f2ecdeecbc316452ef6d4f39cb
Harry <harry811016@gmail.com>
2020-11-04 20:36:16 +0800

    A19pass

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,11 @@
 const bcrypt = require("bcryptjs");
 const db = require("../models");
 const User = db.User;
+const Comment = db.Comment;
+const Restaurant = db.Restaurant;
+const imgur = require("imgur-node-api");
+const IMGUR_CLIENT_ID = "49e3187e6d178f9";
+const helpers = require("../_helpers");
 
 const userController = {
   signUpPage: (req, res) => {
@@ -49,6 +54,77 @@ const userController = {
     req.flash("success_messages", "登出成功！");
     req.logout();
     res.redirect("/signin");
+  },
+  getUser: (req, res) => {
+    if (Number(req.params.id) === helpers.getUser(req).id) {
+      return User.findByPk(req.params.id, {
+        include: { model: Comment, include: [Restaurant] },
+      }).then((user) => {
+        let totalComments = user.dataValues.Comments.length;
+        return res.render("user", {
+          user: user.toJSON(),
+          totalComments: totalComments,
+        });
+      });
+    } else {
+      req.flash("error_messages", "沒有權限進入他人的個人資訊頁面！");
+      return res.redirect(`/users/${helpers.getUser(req).id}`);
+    }
+  },
+  editUser: (req, res) => {
+    if (Number(req.params.id) === helpers.getUser(req).id) {
+      return User.findByPk(req.params.id).then((user) => {
+        return res.render("editUser", {
+          user: user.toJSON(),
+        });
+      });
+    } else {
+      req.flash("error_messages", "沒有權限進入他人修改個人資訊頁面！");
+      return res.redirect(`/users/${helpers.getUser(req).id}`);
+    }
+  },
+  putUser: (req, res) => {
+    if (Number(req.params.id) === helpers.getUser(req).id) {
+      if (!req.body.name) {
+        req.flash("error_messages", "name didn't exist");
+        return res.redirect("back");
+      }
+
+      const { file } = req;
+      if (file) {
+        imgur.setClientID(IMGUR_CLIENT_ID);
+        imgur.upload(file.path, (err, img) => {
+          return User.findByPk(req.params.id).then((user) => {
+            user
+              .update({
+                name: req.body.name,
+                image: file ? img.data.link : restaurant.image,
+              })
+              .then((user) => {
+                req.flash(
+                  "success_messages",
+                  "user was successfully to update"
+                );
+                res.redirect(`/users/${helpers.getUser(req).id}`);
+              });
+          });
+        });
+      } else
+        return User.findByPk(req.params.id).then((user) => {
+          user
+            .update({
+              name: req.body.name,
+              image: user.image,
+            })
+            .then((user) => {
+              req.flash("success_messages", "user was successfully to update");
+              res.redirect(`/users/${helpers.getUser(req).id}`);
+            });
+        });
+    } else {
+      req.flash("error_messages", "沒有權限修改他人資訊！");
+      return res.redirect(`/users/${helpers.getUser(req).id}`);
+    }
   },
 };
 
