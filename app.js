a32808c0bf2360428eec691c3d3c24fdd1d3bd62
Derek Hsu <derek800109@gmail.com>
2020-11-29 06:44:09 +0800

    debug: user data is missing on restaurant page

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const db = require('./models')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
-const session = require('express-session')
-const passport = require('./config/passport')
+const helpers = require('./_helpers')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+
+// -----------------------------------------------------------------------------------
+
+const session = require('express-session')
+const passport = require('./config/passport')
 
 // -----------------------------------------------------------------------------------
 
@@ -18,20 +22,17 @@ const PORT = process.env.PORT || 3000
 
 // -----------------------------------------------------------------------------------
 
-app.engine('handlebars', handlebars({ defaultLayout: 'main' })) // Handlebars 註冊樣板引擎
-app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
-
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
 app.use(methodOverride('_method'))
-
-// 這是因為我們剛剛建立了 upload 資料夾來存放圖片，可是卻沒有設定 express 的路由，因此外界沒辦法存取到 / upload 這個路徑. 設定靜態檔案路徑 /upload
 app.use('/upload', express.static(__dirname + '/upload'))
 
 // -----------------------------------------------------------------------------------
-
-app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
-app.use(flash())
 
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
@@ -39,11 +40,6 @@ app.use((req, res, next) => {
   res.locals.user = req.user
   next()
 })
-
-// -----------------------------------------------------------------------------------
-
-app.use(passport.initialize())
-app.use(passport.session())
 
 // -----------------------------------------------------------------------------------
 
