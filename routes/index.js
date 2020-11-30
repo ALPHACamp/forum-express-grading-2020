f637be88da37b98f743e673d859fcf41c2b55608
Liv Huang <livhuang95@gmail.com>
2020-11-30 14:09:41 +0800

    [Assignment Preparation] ensureAuthenticated & getUser

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+const helpers = require('../_helpers');
+
 const passport = require('passport');
 const multer = require('multer');
 
@@ -9,14 +11,14 @@ const userController = require('../controllers/userController.js');
 
 module.exports = (app) => {
   const authenticated = (req, res, next) => {
-    if (req.isAuthenticated()) {
+    if (helpers.ensureAuthenticated(req)) {
       return next();
     }
     res.redirect('/signin');
   };
   const authenticatedAdmin = (req, res, next) => {
-    if (req.isAuthenticated()) {
-      if (req.user.isAdmin) {
+    if (helpers.ensureAuthenticated(req)) {
+      if (helpers.getUser(req).isAdmin) {
         return next();
       }
       return res.redirect('/');
