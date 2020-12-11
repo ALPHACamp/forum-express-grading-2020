8a1f8263dbe2ef48e68976675061e123eda3d467
Liv Huang <livhuang95@gmail.com>
2020-12-11 21:10:06 +0800

    modify routes

--- routes/index.js
+++ routes/index.js
@@ -1,134 +1,141 @@
-const helpers = require('../_helpers');
+const helpers = require("../_helpers");
 
-const passport = require('passport');
-const multer = require('multer');
+const passport = require("passport");
+const multer = require("multer");
 
-const upload = multer({ dest: 'temp/' });
+const upload = multer({ dest: "temp/" });
 
-const restController = require('../controllers/restController.js');
-const adminController = require('../controllers/adminController.js');
-const userController = require('../controllers/userController.js');
-const categoryController = require('../controllers/categoryController.js');
-const commentController = require('../controllers/commentController.js');
+const restController = require("../controllers/restController.js");
+const adminController = require("../controllers/adminController.js");
+const userController = require("../controllers/userController.js");
+const categoryController = require("../controllers/categoryController.js");
+const commentController = require("../controllers/commentController.js");
 
 module.exports = (app) => {
   const authenticated = (req, res, next) => {
     if (helpers.ensureAuthenticated(req)) {
       return next();
     }
-    res.redirect('/signin');
+    res.redirect("/signin");
   };
   const authenticatedAdmin = (req, res, next) => {
     if (helpers.ensureAuthenticated(req)) {
       if (helpers.getUser(req).isAdmin) {
         return next();
       }
-      return res.redirect('/');
+      return res.redirect("/");
     }
-    res.redirect('/signin');
+    res.redirect("/signin");
   };
 
-  app.get(
-    '/',
-    // authenticated,
-    (req, res) => {
-      res.redirect('/restaurants');
-    }
-  );
-  app.get(
-    '/restaurants',
-    // authenticated,
-    restController.getRestaurants
-  );
+  app.get("/", authenticated, (req, res) => {
+    res.redirect("/restaurants");
+  });
+  app.get("/restaurants", authenticated, restController.getRestaurants);
 
-  app.get(
-    '/restaurants/:id',
-    // authenticated,
-    restController.getRestaurant
-  );
+  app.get("/restaurants/:id", authenticated, restController.getRestaurant);
 
-  app.get(
-    '/admin',
-    // authenticatedAdmin,
-    (req, res) => res.redirect('/admin/restaurants')
+  app.get("/admin", authenticatedAdmin, (req, res) =>
+    res.redirect("/admin/restaurants")
   );
   app.get(
-    '/admin/restaurants',
-    // authenticatedAdmin,
+    "/admin/restaurants",
+    authenticatedAdmin,
     adminController.getRestaurants
   );
   app.get(
-    '/admin/restaurants/create',
-    // authenticatedAdmin,
+    "/admin/restaurants/create",
+    authenticatedAdmin,
     adminController.createRestaurant
   );
   app.post(
-    '/admin/restaurants',
-    // authenticatedAdmin,
-    upload.single('image'),
+    "/admin/restaurants",
+    authenticatedAdmin,
+    upload.single("image"),
     adminController.postRestaurant
   );
   app.get(
-    '/admin/restaurants/:id',
-    // authenticatedAdmin,
+    "/admin/restaurants/:id",
+    authenticatedAdmin,
     adminController.getRestaurant
   );
   app.get(
-    '/admin/restaurants/:id/edit',
-    // authenticatedAdmin,
+    "/admin/restaurants/:id/edit",
+    authenticatedAdmin,
     adminController.editRestaurant
   );
   app.put(
-    '/admin/restaurants/:id',
-    // authenticatedAdmin,
-    upload.single('image'),
+    "/admin/restaurants/:id",
+    authenticatedAdmin,
+    upload.single("image"),
     adminController.putRestaurant
   );
   app.delete(
-    '/admin/restaurants/:id',
-    // authenticatedAdmin,
+    "/admin/restaurants/:id",
+    authenticatedAdmin,
     adminController.deleteRestaurant
   );
 
-  app.get('/signup', userController.signUpPage);
-  app.post('/signup', userController.signUp);
+  app.get("/signup", userController.signUpPage);
+  app.post("/signup", userController.signUp);
 
-  app.get('/signin', userController.signInPage);
+  app.get("/signin", userController.signInPage);
   app.post(
-    '/signin',
-    passport.authenticate('local', {
-      failureRedirect: '/signin',
+    "/signin",
+    passport.authenticate("local", {
+      failureRedirect: "/signin",
       failureFlash: true,
     }),
     userController.signIn
   );
-  app.get('/logout', userController.logout);
+  app.get("/logout", userController.logout);
 
-  app.get(
-    '/admin/users',
-    // authenticatedAdmin,
-    adminController.getUsers
-  );
+  app.get("/admin/users", authenticatedAdmin, adminController.getUsers);
   app.put(
-    '/admin/users/:id/toggleAdmin',
-    // authenticatedAdmin,
+    "/admin/users/:id/toggleAdmin",
+    authenticatedAdmin,
     adminController.putUsers
   );
 
-  app.get('/admin/categories', categoryController.getCategories);
-  app.post('/admin/categories', categoryController.postCategory);
-  app.delete('/admin/categories/:id', categoryController.deleteCategory);
-  app.get('/admin/categories/:id', categoryController.getCategories);
-  app.put('/admin/categories/:id', categoryController.putCategory);
-
-  app.post('/comments', commentController.postComment);
+  app.get(
+    "/admin/categories",
+    authenticatedAdmin,
+    categoryController.getCategories
+  );
+  app.post(
+    "/admin/categories",
+    authenticatedAdmin,
+    categoryController.postCategory
+  );
   app.delete(
-    '/comments/:id',
+    "/admin/categories/:id",
+    authenticatedAdmin,
+    categoryController.deleteCategory
+  );
+  app.get(
+    "/admin/categories/:id",
+    authenticatedAdmin,
+    categoryController.getCategories
+  );
+  app.put(
+    "/admin/categories/:id",
+    authenticatedAdmin,
+    categoryController.putCategory
+  );
+
+  app.post("/comments", authenticated, commentController.postComment);
+  app.delete(
+    "/comments/:id",
     authenticatedAdmin,
     commentController.deleteComment
   );
 
-  app.get('/users/:id', userController.getUser);
-  app.get('/users/:id/edit', userController.editUser);
-  app.put('/users/:id', upload.single('userImage'), userController.putUser);
+  app.get("/users/:id", authenticated, userController.getUser);
+  app.get("/users/:id/edit", authenticated, userController.editUser);
+  app.put(
+    "/users/:id",
+    authenticated,
+    upload.single("userImage"),
+    userController.putUser
+  );
 };
