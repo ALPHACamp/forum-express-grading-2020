0beedb6ae1ba5a8f0a9fe4ef4d54e356617204fe
Derek Hsu <derek800109@gmail.com>
2020-12-14 22:55:30 +0800

    feat: POST: /api/admin/restaurants

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.engine('handlebars', handlebars({
 app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(passport.initialize())
