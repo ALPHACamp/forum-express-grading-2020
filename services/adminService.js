d36220e1d4a25bda810167fc559f618037827f2b
Derek Hsu <derek800109@gmail.com>
2020-12-14 22:05:41 +0800

    debug: fix raw/nest issue

--- services/adminService.js
+++ services/adminService.js
@@ -6,15 +6,14 @@ const Category = db.Category
 
 const adminService = {
   getRestaurants: (req, res, callback) => {
-    return Restaurant.findAll({ include: [Category] }).then(restaurants => {
+    return Restaurant.findAll({ raw: true, nest: true, include: [Category] }).then(restaurants => {
       callback({ restaurants: restaurants })
     })
   },
 
-
   getRestaurant: (req, res, callback) => {
     return Restaurant.findByPk(req.params.id, { include: [Category] }).then(restaurant => {
-      callback({ restaurant: restaurant })
+      callback({ restaurant: restaurant.toJSON() })
     })
   },
 }
