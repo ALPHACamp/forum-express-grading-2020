fa01abb6b800fe9b7493f0e11adda2f9f705e7a9
Jackson Hsieh <jackson97042@gmail.com>
2021-01-30 20:47:29 +0800

    feat: install bcryptjs; add userController and signup page; install bodyparser;

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const hbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 const PORT = 3000
 
 app.engine('hbs', hbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(PORT, () => {
   console.log(`The server is listening to http://localhost:${PORT}.`)
