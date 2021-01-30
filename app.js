d5a4755c418e74ff8f067b71a13e471cf1e91871
Thumper <shaunwhl1991@gmail.com>
2021-01-31 00:17:48 +1100

    add urlencoded to parse application/x-www-form-urlencoded
    add urlencoded to parse application/x-www-form-urlencoded

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port = 3000;
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' })); // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars'); // 設定使用 Handlebars 做為樣板引擎
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
