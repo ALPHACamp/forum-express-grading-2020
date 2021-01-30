024ab1432db24b69cef22510695c2933851647bd
Jackson Hsieh <jackson97042@gmail.com>
2021-01-30 21:39:03 +0800

    feat: install express-session connect-flash

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const hbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const flash = require('connect-flash')
 const app = express()
 const PORT = 3000
 
@@ -8,6 +10,14 @@ app.engine('hbs', hbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(PORT, () => {
   console.log(`The server is listening to http://localhost:${PORT}.`)
