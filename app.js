6ee8a23272d8c7ce85682168982050cfa814800d
thumperL <shaunwhl1991@gmail.com>
2021-01-31 13:44:16 +1100

    Confirm user password and unique email
    Confirm user password and unique email

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const flash = require('connect-flash');
+const session = require('express-session');
 const db = require('./models');
 
 const app = express();
@@ -8,6 +10,15 @@ const port = 3000;
 app.engine('handlebars', handlebars({ defaultLayout: 'main' })); // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars'); // 設定使用 Handlebars 做為樣板引擎
 app.use(express.urlencoded({ extended: false }));
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }));
+app.use(flash());
+
+// 把 req.flash 放到 res.locals 裡面
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages');
+  res.locals.error_messages = req.flash('error_messages');
+  next();
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- controllers/userController.js
+++ controllers/userController.js
@@ -7,13 +7,26 @@ const userController = {
   signUpPage: (req, res) => res.render('signup'),
 
   signUp: (req, res) => {
-    console.log(req);
-
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null),
-    }).then((user) => res.redirect('/signin'));
+    // confirm password
+    if (req.body.passwordCheck !== req.body.password) {
+      req.flash('error_messages', '兩次密碼輸入不同！');
+      return res.redirect('/signup');
+    }
+    // confirm unique user
+    User.findOne({ where: { email: req.body.email } }).then((user) => {
+      if (user) {
+        req.flash('error_messages', '信箱重複！');
+        return res.redirect('/signup');
+      }
+      User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null),
+      }).then((user) => {
+        req.flash('success_messages', '成功註冊帳號！');
+        return res.redirect('/signin');
+      });
+    });
   },
 };
 
