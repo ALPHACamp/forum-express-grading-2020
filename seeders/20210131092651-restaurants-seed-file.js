d41a93fcf02c96c41be99795fc22bc25e7efc49b
Thumper <shaunwhl1991@gmail.com>
2021-02-04 15:57:55 +1100

    Add category seed data
    Add category seed data

--- seeders/20210131092651-restaurants-seed-file.js
+++ seeders/20210131092651-restaurants-seed-file.js
@@ -4,14 +4,15 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Restaurants',
       Array.from({ length: 50 }).map((d, i) => ({
-        name: faker.name.findName(),
-        tel: faker.phone.phoneNumber(),
-        address: faker.address.streetAddress(),
+        name         : faker.name.findName(),
+        tel          : faker.phone.phoneNumber(),
+        address      : faker.address.streetAddress(),
         opening_hours: '08:00',
-        image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
-        description: faker.lorem.text(),
-        createdAt: new Date(),
-        updatedAt: new Date(),
+        image        : `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
+        description  : faker.lorem.text(),
+        createdAt    : new Date(),
+        updatedAt    : new Date(),
+        CategoryId   : Math.floor(Math.random() * 7) * 10 + 1,
       })), {});
   },
 
