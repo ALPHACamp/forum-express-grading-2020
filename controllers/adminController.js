b917038c4a3758a9fd27208d817c05e8233e81e0
Jackson Hsieh <jackson97042@gmail.com>
2021-02-05 11:09:04 +0800

    feat: add category select in edit page using ifCond-helper

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -46,6 +46,7 @@ const adminController = {
           opening_hours: req.body.opening_hours,
           description: req.body.description,
           image: file ? img.data.link : null,
+          CategoryId: req.body.categoryId
         }).then((restaurant) => {
           req.flash('success_messages', 'restaurant was successfully created')
           return res.redirect('/admin/restaurants')
@@ -59,7 +60,8 @@ const adminController = {
         address: req.body.address,
         opening_hours: req.body.opening_hours,
         description: req.body.description,
-        image: null
+        image: null,
+        CategoryId: req.body.categoryId
       }).then((restaurant) => {
         req.flash('success_messages', 'restaurant was successfully created')
         return res.redirect('/admin/restaurants')
@@ -110,6 +112,7 @@ const adminController = {
               opening_hours: req.body.opening_hours,
               description: req.body.description,
               image: file ? img.data.link : restaurant.image,
+              CategoryId: req.body.categoryId
             })
             .then((restaurant) => {
               req.flash('success_messages', 'restaurant was successfully to update')
@@ -127,7 +130,8 @@ const adminController = {
             address: req.body.address,
             opening_hours: req.body.opening_hours,
             description: req.body.description,
-            image: restaurant.image
+            image: restaurant.image,
+            CategoryId: req.body.categoryId
           })
           .then((restaurant) => {
             req.flash('success_messages', 'restaurant was successfully to update')
