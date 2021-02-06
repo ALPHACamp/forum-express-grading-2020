d996745508ca64d35ffe70eb69a2f5538ce4f4f3
Jackson Hsieh <jackson97042@gmail.com>
2021-02-06 21:35:49 +0800

    fix: change comments-seed foreign-key id

--- seeders/0-20210206045332-comments-seed-file.js
+++ seeders/0-20210206045332-comments-seed-file.js
@@ -4,22 +4,22 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Comments', [{
         text: '會想再來!!',
-        UserId: 4,
-        RestaurantId: 203,
+        UserId: 1,
+        RestaurantId: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         text: '難吃到爆!!',
-        UserId: 4,
-        RestaurantId: 204,
+        UserId: 1,
+        RestaurantId: 11,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         text: '米其林1星!!',
-        UserId: 4,
-        RestaurantId: 203,
+        UserId: 1,
+        RestaurantId: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       }], {})
