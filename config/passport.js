759ede770f7d29dacd45cc958a65027a47ce106a
Jackson Hsieh <jackson97042@gmail.com>
2021-02-10 22:28:24 +0800

    feat: Switch add/remove favorite button on restaurants index page

--- config/passport.js
+++ config/passport.js
@@ -3,7 +3,7 @@ const LocalStrategy = require('passport-local')
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
-
+const Restaurant = db.Restaurant
 // setup passport strategy
 passport.use(new LocalStrategy(
   // customize user field
@@ -27,8 +27,12 @@ passport.serializeUser((user, cb) => {
   cb(null, user.id)
 })
 passport.deserializeUser((id, cb) => {
-  User.findByPk(id).then(user => {
-    user = user.toJSON() // 此處與影片示範不同
+  User.findByPk(id, {
+    include: [
+      { model: Restaurant, as: 'FavoritedRestaurants' }
+    ]
+  }).then(user => {
+    user = user.toJSON()
     return cb(null, user) 
   })
 })
