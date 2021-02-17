06cef3e62eb55aac48c93a2dbf46190c7a3a747e
Jackson Hsieh <jackson97042@gmail.com>
2021-02-17 13:04:38 +0800

    feat & refactor: A26 PUT /api/admin/restaurants/:id - 後台修改個別餐廳

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -36,45 +36,7 @@ const adminController = {
       }
     })
    },
-  // postRestaurant: (req, res) => {
-  //   if (!req.body.name) {
-  //     req.flash('error_messages', "name didn't exist")
-  //     return res.redirect('back')
-  //   }
-
-  //   const { file } = req
-  //   if (file) {
-  //     imgur.setClientID(IMGUR_CLIENT_ID);
-  //     imgur.upload(file.path, (err, img) => {
-  //       return Restaurant.create({
-  //         name: req.body.name,
-  //         tel: req.body.tel,
-  //         address: req.body.address,
-  //         opening_hours: req.body.opening_hours,
-  //         description: req.body.description,
-  //         image: file ? img.data.link : null,
-  //         CategoryId: req.body.categoryId
-  //       }).then((restaurant) => {
-  //         req.flash('success_messages', 'restaurant was successfully created')
-  //         return res.redirect('/admin/restaurants')
-  //       })
-  //     })
-  //   }
-  //   else {
-  //     return Restaurant.create({
-  //       name: req.body.name,
-  //       tel: req.body.tel,
-  //       address: req.body.address,
-  //       opening_hours: req.body.opening_hours,
-  //       description: req.body.description,
-  //       image: null,
-  //       CategoryId: req.body.categoryId
-  //     }).then((restaurant) => {
-  //       req.flash('success_messages', 'restaurant was successfully created')
-  //       return res.redirect('/admin/restaurants')
-  //     })
-  //    }
-  // },
+  
 
   getRestaurant: (req, res) => {
     adminService.getRestaurant(req, res, (data) => {
@@ -97,52 +59,64 @@ const adminController = {
   },
 
   putRestaurant: (req, res) => {
-    if(!req.body.name){
-      req.flash('error_messages', "name didn't exist")
-      return res.redirect('back')
-    }
+    adminService.putRestaurant(req, res, (data) => {
+      if (data['status'] === 'error') {
+        req.flash('error_messages', data['message'])
+        return res.redirect('back') 
+      } else if (data['status'] === 'success') {
+        req.flash('success_messages', data['message'])
+        return res.redirect('/admin/restaurants') 
+      }
+    })
+   },
+
+  // putRestaurant: (req, res) => {
+  //   if(!req.body.name){
+  //     req.flash('error_messages', "name didn't exist")
+  //     return res.redirect('back')
+  //   }
   
-    const { file } = req
-    if (file) {
-      imgur.setClientID(IMGUR_CLIENT_ID);
-      imgur.upload(file.path, (err, img) => {
-        return Restaurant.findByPk(req.params.id)
-          .then((restaurant) => {
-            restaurant.update({
-              name: req.body.name,
-              tel: req.body.tel,
-              address: req.body.address,
-              opening_hours: req.body.opening_hours,
-              description: req.body.description,
-              image: file ? img.data.link : restaurant.image,
-              CategoryId: req.body.categoryId
-            })
-            .then((restaurant) => {
-              req.flash('success_messages', 'restaurant was successfully to update')
-              res.redirect('/admin/restaurants')
-            })
-          })
-      })
-    }
-    else {
-      return Restaurant.findByPk(req.params.id)
-        .then((restaurant) => {
-          restaurant.update({
-            name: req.body.name,
-            tel: req.body.tel,
-            address: req.body.address,
-            opening_hours: req.body.opening_hours,
-            description: req.body.description,
-            image: restaurant.image,
-            CategoryId: req.body.categoryId
-          })
-          .then((restaurant) => {
-            req.flash('success_messages', 'restaurant was successfully to update')
-            res.redirect('/admin/restaurants')
-          })
-        })
-    }
-  },
+  //   const { file } = req
+  //   if (file) {
+  //     imgur.setClientID(IMGUR_CLIENT_ID);
+  //     imgur.upload(file.path, (err, img) => {
+  //       return Restaurant.findByPk(req.params.id)
+  //         .then((restaurant) => {
+  //           restaurant.update({
+  //             name: req.body.name,
+  //             tel: req.body.tel,
+  //             address: req.body.address,
+  //             opening_hours: req.body.opening_hours,
+  //             description: req.body.description,
+  //             image: file ? img.data.link : restaurant.image,
+  //             CategoryId: req.body.categoryId
+  //           })
+  //           .then((restaurant) => {
+  //             req.flash('success_messages', 'restaurant was successfully to update')
+  //             res.redirect('/admin/restaurants')
+  //           })
+  //         })
+  //     })
+  //   }
+  //   else {
+  //     return Restaurant.findByPk(req.params.id)
+  //       .then((restaurant) => {
+  //         restaurant.update({
+  //           name: req.body.name,
+  //           tel: req.body.tel,
+  //           address: req.body.address,
+  //           opening_hours: req.body.opening_hours,
+  //           description: req.body.description,
+  //           image: restaurant.image,
+  //           CategoryId: req.body.categoryId
+  //         })
+  //         .then((restaurant) => {
+  //           req.flash('success_messages', 'restaurant was successfully to update')
+  //           res.redirect('/admin/restaurants')
+  //         })
+  //       })
+  //   }
+  // },
 
   deleteRestaurant: (req, res) => {
     adminService.deleteRestaurant(req, res, (data) => {
--- routes/apis.js
+++ routes/apis.js
@@ -9,8 +9,9 @@ const upload = multer({ dest: 'temp/' })
 
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
-router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
+router.put('/admin/restaurants/:id', upload.single('image'), adminController.putRestaurant)
+router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 
 router.get('/admin/categories', categoryController.getCategories)
 
