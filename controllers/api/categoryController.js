f00d3edd62cae707646adf78f3701e422c2f5c18
Jackson Hsieh <jackson97042@gmail.com>
2021-02-17 14:17:13 +0800

    feat: A26 DELETE /api/admin/categories/:id - 後台刪除類別

--- controllers/api/categoryController.js
+++ controllers/api/categoryController.js
@@ -21,6 +21,12 @@ const categoryController = {
     })
   },
 
+  deleteCategory: (req, res) => {
+    categoryService.deleteCategory(req, res, (data) => {
+      return res.json(data)
+    })
+  }
+
 }
 
 module.exports = categoryController
--- services/categoryService.js
+++ services/categoryService.js
@@ -48,6 +48,16 @@ const categoryService = {
     }
   },
 
+  deleteCategory: (req, res, callback) => {
+    return Category.findByPk(req.params.id)
+      .then((category) => {
+        category.destroy()
+          .then((category) => {
+            return callback({  status: 'success', message: 'Category was deleted successfully.' })
+          })
+      })
+  }
+
 }
 
 module.exports = categoryService
