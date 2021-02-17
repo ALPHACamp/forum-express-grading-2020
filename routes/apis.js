55c8fddcde9e34c5b4a7fb131a852e043affac57
Jackson Hsieh <jackson97042@gmail.com>
2021-02-17 16:55:33 +0800

    feat: passport-jwt & POST /api/signin (return jwt)

--- routes/apis.js
+++ routes/apis.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController.js')
+const userController = require('../controllers/api/userController.js')
 
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
@@ -17,6 +18,8 @@ router.get('/admin/categories', categoryController.getCategories)
 router.post('/admin/categories', categoryController.postCategory)
 router.put('/admin/categories/:id', categoryController.putCategory)
 router.delete('/admin/categories/:id', categoryController.deleteCategory)
+// JWT signin
+router.post('/signin', userController.signIn)
 
 
 module.exports = router
