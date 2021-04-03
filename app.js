0ca455256bf3bc70fcf6f102313fffd5f1cd7b71
Ben Chou <ben7152000@gmail.com>
2021-04-04 00:24:20 +0800

    create server connect, view, router

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+const userPassport = require('./config/passport')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+const app = express()
+const PORT = process.env.PORT || 3000
+
+// eslint-disable-next-line node/no-path-concat
+app.use('/upload', express.static(__dirname + '/upload'))
+
+app.engine('handlebars', handlebars())
+app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.use(session({ secret: 'Secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use(methodOverride('_method'))
+
+userPassport(app)
+
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
 })
 
 require('./routes')(app)
+
+app.listen(PORT, () => {
+  console.log(`The server is running on localhost:${PORT}`)
+})
 
 module.exports = app
