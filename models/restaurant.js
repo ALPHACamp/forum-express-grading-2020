e73a95fe1c410341e1a7315deb82db80a9af2a0e
Ben Chou <ben7152000@gmail.com>
2021-04-08 12:41:59 +0800

    add categoty schema

--- models/restaurant.js
+++ models/restaurant.js
@@ -3,6 +3,7 @@ const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Restaurant extends Model {
     static associate (models) {
+      Restaurant.belongsTo(models.Category)
     }
   };
   Restaurant.init({
@@ -11,7 +12,8 @@ module.exports = (sequelize, DataTypes) => {
     address: DataTypes.STRING,
     opening_hours: DataTypes.STRING,
     description: DataTypes.TEXT,
-    image: DataTypes.STRING
+    image: DataTypes.STRING,
+    CategoryId: DataTypes.INTEGER
   }, {
     sequelize,
     modelName: 'Restaurant'
