7b3765ea55938bac9911ed04eb7f92412ae7523c
Ben Chou <ben7152000@gmail.com>
2021-04-09 17:40:24 +0800

    add category filter feature

--- controllers/restController.js
+++ controllers/restController.js
@@ -6,14 +6,22 @@ const restController = {
 
   // 顯示全部餐廳
   getRestaurants: async (req, res) => {
+    const id = req.query.categoryId
+    const querystring = {}
+    let categoryId = ''
+    if (id) {
+      categoryId = Number(id)
+      querystring.CategoryId = categoryId
+    }
     try {
-      const restaurants = await Restaurant.findAll({ include: Category })
+      const restaurants = await Restaurant.findAll({ include: Category, where: querystring })
+      const categories = await Category.findAll({ raw: true, nest: true })
       const data = restaurants.map(restaurant => ({
         ...restaurant.dataValues,
         description: restaurant.dataValues.description.substring(0, 50),
         categoryName: restaurant.Category.name
       }))
-      return res.render('restaurants', { restaurants: data })
+      return res.render('restaurants', { restaurants: data, categories, categoryId })
     } catch (e) {
       console.log(e)
     }
