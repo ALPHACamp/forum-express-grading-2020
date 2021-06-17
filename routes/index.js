480eeaabc9ac9130845d65ef866109fa2e731840
Nina19980108 <ninagirl8787@gmail.com>
2021-06-17 13:40:44 +0800

    ensureAuthenticated & getUser

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,19 @@
 const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController.js')
+const helpers = require('../_helpers')
 const passport = require('passport')
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
 
 module.exports = app => {
   const authenticated = (req, res, next) => {
-    if (req.isAuthenticated()) return next()
+    if (helpers.ensureAuthenticated(req)) return next()
     res.redirect('/signin')
   }
   const authenticatedAdmin = (req, res, next) => {
-    if (req.isAuthenticated()) {
-      if (req.user.isAdmin) return next()
+    if (helpers.ensureAuthenticated(req)) {
+      if (helpers.getUser(req).isAdmin) return next()
       return res.redirect('/')
     }
     res.redirect('/signin')
