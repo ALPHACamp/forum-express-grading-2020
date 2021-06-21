b27906e7bd34aa10ea6d97a674b6363e7cdcbe2b
Nina19980108 <ninagirl8787@gmail.com>
2021-06-21 16:53:25 +0800

    feat: show category name in restaurant & restaurants hbs

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,13 +1,18 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const User = db.User
+const Category = db.Category
 const fs = require('fs')
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({ raw: true })
+    return Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: [Category]
+    })
       .then(restaurants => {
         res.render('admin/restaurants', { restaurants })
       })
@@ -56,9 +61,11 @@ const adminController = {
   },
 
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { raw: true })
+    return Restaurant.findByPk(req.params.id, {
+      include: [Category]
+    })
       .then(restaurant => {
-        return res.render('admin/restaurant', { restaurant })
+        return res.render('admin/restaurant', { restaurant: restaurant.toJSON() })
       })
   },
 
