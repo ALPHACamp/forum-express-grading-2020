74519a63045dfa5c016d1f1f9b545ed47999d690
area268457913 <area268457913@hotmail.com>
2021-06-22 17:36:18 +0800

    Install bcryptjs & Add userController and signup page & Install body-parser

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const handlebars = require('express-handlebars') // 引入 handlebars
 const db = require('./models')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-
+app.use(bodyParser.urlencoded({ extended: true }))
 app.engine('handlebars', handlebars()) // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
 
