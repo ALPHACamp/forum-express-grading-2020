438d8f8e0bde7a8cacaf29e17b148e4df4e107a1
Nina19980108 <ninagirl8787@gmail.com>
2021-06-25 11:02:23 +0800

    feat: add feeds page

--- controllers/restController.js
+++ controllers/restController.js
@@ -61,6 +61,30 @@ const restController = {
           restaurant: restaurant.toJSON()
         })
       })
-  }
+  },
+
+  getFeeds: (req, res) => {
+    return Promise.all([
+      Restaurant.findAll({
+        limit: 10,
+        raw: true,
+        nest: true,
+        order: [['createdAt', 'Desc']],
+        include: [Category]
+      }),
+      Comment.findAll({
+        limit: 10,
+        raw: true,
+        nest: true,
+        order: [['createdAt', 'Desc']],
+        include: [User, Restaurant]
+      })
+    ]).then(([restaurants, comments]) => {
+      return res.render('feeds', {
+        restaurants,
+        comments
+      })
+    })
+  },
 }
 module.exports = restController
