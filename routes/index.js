55d4f01e8a75fe7eda13f313e74a88919876aec2
Nina19980108 <ninagirl8787@gmail.com>
2021-06-27 10:37:23 +0800

    feat: Add topUser page

--- routes/index.js
+++ routes/index.js
@@ -34,6 +34,7 @@ module.exports = app => {
   app.post('/comments', authenticated, commentController.postComment)
   app.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 
+  app.get('/users/top', authenticated, userController.getTopUser)
   app.get('/users/:id', authenticated, userController.getUser)
   app.get('/users/:id/edit', authenticated, userController.editUser)
   app.put('/users/:id', authenticated, authenticatedUser, upload.single('image'), userController.putUser)
