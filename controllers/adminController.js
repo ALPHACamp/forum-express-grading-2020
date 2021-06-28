914ffe299c076f3d1348408f6dca5b0bedfa71b3
Nina19980108 <ninagirl8787@gmail.com>
2021-06-28 11:50:43 +0800

    refactor: add adminService to modify adminController

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,16 +6,13 @@ const fs = require('fs')
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
+const adminService = require('../services/adminService')
+
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({
-      raw: true,
-      nest: true,
-      include: [Category]
+    adminService.getRestaurants(req, res, (data) => {
+      return res.render('admin/restaurants', data)
     })
-      .then(restaurants => {
-        res.render('admin/restaurants', { restaurants })
-      })
   },
 
   createRestaurant: (req, res) => {
