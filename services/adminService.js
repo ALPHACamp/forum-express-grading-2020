2088944084fe9fc3d5deae4135a5e4fd36e83196
Nina19980108 <ninagirl8787@gmail.com>
2021-06-28 12:01:17 +0800

    feat: add GET api/admin/restaurants/:id router

--- services/adminService.js
+++ services/adminService.js
@@ -13,6 +13,15 @@ const adminService = {
         callback({ restaurants })
       })
   },
+
+  getRestaurant: (req, res, callback) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: [Category]
+    })
+      .then(restaurant => {
+        callback({ restaurant: restaurant.toJSON() })
+      })
+  },
 }
 
 module.exports = adminService
