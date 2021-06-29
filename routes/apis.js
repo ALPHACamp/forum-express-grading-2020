d268309083ce5a035fda19c0a733b4e019c08d85
Nina19980108 <ninagirl8787@gmail.com>
2021-06-29 16:03:15 +0800

    feat: add PUT /api/admin/restaurants/:id

--- routes/apis.js
+++ routes/apis.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
+
 const adminController = require('../controllers/api/adminController.js')
 
 router.get('/admin/restaurants', adminController.getRestaurants)
@@ -8,4 +11,5 @@ router.get('/admin/restaurants/:id', adminController.getRestaurant)
 router.get('/admin/categories', adminController.getCategories)
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 
+router.put('/admin/restaurants/:id', upload.single('image'), adminController.putRestaurant)
 module.exports = router
