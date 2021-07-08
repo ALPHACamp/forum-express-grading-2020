2315d10c48d804b9ff45fad8be9269ff680494dc
Yu-Ting Wu <a22627046@gmail.com>
2021-07-09 00:16:10 +0800

    Add comments seeder

---
+++ seeders/20210708150820-comments-seed-file.js
@@ -0,0 +1,18 @@
+'use strict';
+const faker = require('faker')
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Comments',
+      Array.from({ length: 20 }).map((d, i) => ({
+        text: faker.lorem.sentence(),
+        UserId: Math.floor(Math.random() * 3) + 1,
+        RestaurantId: Math.floor(Math.random() * 20) + 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })), {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Comments', null, {})
+  }
+};
