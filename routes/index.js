eaa1c2d327ce010dd976051681122f54dbaa7370
Trevor Lin <trevor0966@gmail.com>
2021-08-25 16:00:47 +0800

    Add isAdmin to users table & Add authentication before rendering pages

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,37 @@ const adminController = require('../controllers/adminController')
 const userController = require('../controllers/userController')
 
 module.exports = (app, passport) => {
-  // 如果使用者訪問首頁，就導向 /restaurants 的頁面
-  app.get('/', (req, res) => {
-    res.redirect('/restaurants')
-  })
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) return next()
+    return res.redirect('/signin')
+  }
+
+  const authenticatedAdmin  = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) return next()
+      return res.redirect('/')
+    }
+    return res.redirect('/signin')
+  }
+
   // 在 /restaurants 底下則交給 restController.getRestaurants 來處理
-  app.get('/restaurants', restController.getRestaurants)
+  app.get('/restaurants', authenticated, restController.getRestaurants)
 
   // 連到 /admin 頁面就轉到 /admin/restaurants
-  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
 
   // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
 
+  // 註冊登入登出
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
-
   app.get('/signin', userController.signInPage)
   app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
   app.get('/logout', userController.logout)
+
+  // 如果使用者訪問首頁，就導向 /restaurants 的頁面
+  app.get('/', authenticated, (req, res) => {
+    res.redirect('/restaurants')
+  })
 }
