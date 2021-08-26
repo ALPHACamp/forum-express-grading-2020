7dc39087ffd247f348668b327e89f64b04747b3c
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-08-26 13:19:49 +0800

    feat: Set up express-handlebars into app.js
    1. Set up to use .hbs
    2. Set up view engine to use hbs

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const exphdbs = require('express-handlebars')
 const app = express()
 const port = 3000
+
+// Setting express-handlebars
+app.engine(
+  'hbs',
+  exphdbs({
+    defaultLayout: 'main',
+    extname: '.hbs'
+  })
+)
+app.set('view engine', 'hbs')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
