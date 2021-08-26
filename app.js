d81b9737adcbb289aaa7607dab4847d88ac18aea
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-08-26 22:52:44 +0800

    feat: Set up connect-flash
    1. Set up express-session
    2. Set up req.flash to store into res.locals

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const exphdbs = require('express-handlebars')
+const session = require('express-session')
+const flash = require('connect-flash')
 const db = require('./models')
 const app = express()
 
@@ -21,6 +23,25 @@ app.set('view engine', 'hbs')
 
 // Setting body-parser
 app.use(express.urlencoded({ extended: true }))
+
+// Setting express-session
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+)
+
+// Setting connect-flash
+app.use(flash())
+
+// Setting middleware to store info into res.locals
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
