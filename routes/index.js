14924c5feb2b5ecffa4e3d40ce21ad21e684529b
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-08-27 22:50:26 +0800

    feat: Join authentication

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,41 @@ const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController.js')
 
 module.exports = (app, passport) => {
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/signin')
+  }
+
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) {
+        return next()
+      }
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
   // If the user visits the homepage, it will be directed to the /restaurants page
-  app.get('/', (req, res) => {
+  app.get('/', authenticated, (req, res) => {
     res.redirect('./restaurants')
   })
 
   // '/restaurants' are handed over to restController.get Restaurants
-  app.get('/restaurants', restController.getRestaurants)
+  app.get('/restaurants', authenticated, restController.getRestaurants)
 
   // Connect to the '/admin' page ,it will be directed to the /admin/restaurants
-  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin', authenticatedAdmin, (req, res) =>
+    res.redirect('/admin/restaurants')
+  )
 
   // '/admin/restaurants' are handed over to adminController.getRestaurants
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get(
+    '/admin/restaurants',
+    authenticatedAdmin,
+    adminController.getRestaurants
+  )
 
   app.get('/signup', userController.signUpPage)
 
