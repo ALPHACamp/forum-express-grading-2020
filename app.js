35f63c9d3e60b68a2d9ca5bdb1f8ff909f071374
Trevor Lin <trevor0966@gmail.com>
2021-08-27 23:10:14 +0800

    Install Imgur api and update admin POST and PUT & Add dotenv

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
-const db = require('./models') // 引入資料庫
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
 const methodOverride = require('method-override')
 const app = express()
-const port = process.env.PORT || 3000
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const port = process.env.PORT
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
