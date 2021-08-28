c0b3228eeac49e89f19784b3ca138b0bcac0502e
leo812leo <leo_812leo@hotmail.com.tw>
2021-08-29 01:24:48 +0800

    feat: Restaurant Model

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,9 +1,9 @@
-'use strict'
+'use strict';
 const {
   Model
-} = require('sequelize')
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Restaurant extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,14 +13,15 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   };
-  User.init({
+  Restaurant.init({
     name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    isAdmin: DataTypes.BOOLEAN
+    tel: DataTypes.STRING,
+    address: DataTypes.STRING,
+    opening_hours: DataTypes.STRING,
+    description: DataTypes.TEXT
   }, {
     sequelize,
-    modelName: 'User'
-  })
-  return User
-}
+    modelName: 'Restaurant',
+  });
+  return Restaurant;
+};
