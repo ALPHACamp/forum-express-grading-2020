c1cc6f3d9a25a0c4d715e73d4a7f9ccdc17491b6
leo812leo <leo_812leo@hotmail.com.tw>
2021-08-28 22:01:42 +0800

    feat: userController

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const restController = require('../controllers/restController.js')
-const adminController = require('../controllers/adminController')
+const adminController = require('../controllers/adminController.js')
+const userController = require('../controllers/userController.js')
 module.exports = app => {
   //如果使用者訪問首頁，就導向 /restaurants 的頁面
   app.get('/', (req, res) => res.redirect('/restaurants'))
@@ -9,5 +10,6 @@ module.exports = app => {
   app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
   app.get('/admin/restaurants', adminController.getRestaurants)
 
-
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
 }
