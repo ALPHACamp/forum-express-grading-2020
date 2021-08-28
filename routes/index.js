2c034ac834533a1b99cb25c2c933f594cd368f1a
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-08-28 16:50:32 +0800

    feat: Modify index.js's route path to create new restaurant
    1. route path: '/admin/restaurants', method: POST
    2. Create by using adminController.postRestaurant

--- routes/index.js
+++ routes/index.js
@@ -45,6 +45,12 @@ module.exports = (app, passport) => {
     adminController.createRestaurant
   )
 
+  app.post(
+    '/admin/restaurants',
+    authenticatedAdmin,
+    adminController.postRestaurant
+  )
+
   app.get('/signup', userController.signUpPage)
 
   app.post('/signup', userController.signUp)
