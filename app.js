c48e4873918957c270af8798d194ea8a08349ddd
leo812leo <leo_812leo@hotmail.com.tw>
2021-08-29 01:56:21 +0800

    feat: admin edit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
-// const db = require('./models')
+const methodOverride = require('method-override')
 
 const app = express()
 const port = 3000
@@ -15,6 +15,7 @@ app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
+app.use(methodOverride('_method'))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
