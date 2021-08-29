c90e19d1f7e20df8a7525b0ec93e695f613e538b
leo812leo <leo_812leo@hotmail.com.tw>
2021-08-29 23:45:54 +0800

    heroku PG switch to ClearDB mysql

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ const methodOverride = require('method-override')
 
 const app = express()
 const port = process.env.PORT || 3000
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 app.engine('handlebars', handlebars())
 app.set('view engine', 'handlebars')
-
 app.use('/upload', express.static(__dirname + '/upload'))
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
