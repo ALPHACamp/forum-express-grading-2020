1ded64dd9b5083f649ed6fa8f43ce4d8f89c0219
aoigj100a <aoigj100@gmail.com>
2021-09-01 22:24:59 +0800

    add nav

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController.js')
+const passport = require('passport')
 
 module.exports = app => {
   app.get('/', (req, res) => res.redirect('/restaurants'))
@@ -9,6 +10,7 @@ module.exports = app => {
   app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
  
   app.get('/admin/restaurants', adminController.getRestaurants)
-  app.get('/signup', userController.signUpPage)
-  app.post('/signup', userController.signUp)
+  app.get('/signin', userController.signInPage)
+  app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+  app.get('/logout', userController.logout)
  }
