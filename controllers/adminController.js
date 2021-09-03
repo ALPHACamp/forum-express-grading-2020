184dc04d4a93bda9c5c0fd34f9fe035f8dc3de18
leo812leo <leo_812leo@hotmail.com.tw>
2021-09-04 00:51:10 +0800

    feat: restaurant show Category

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,10 +4,11 @@ const User = db.User
 const fs = require('fs')
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+const Category = db.Category
 
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({ raw: true }).then(restaurants => {
+    return Restaurant.findAll({ raw: true, nest: true, include: [Category] }).then(restaurants => {
       return res.render('admin/restaurants', { restaurants })
     })
   },
@@ -52,8 +53,8 @@ const adminController = {
     }
   },
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { raw: true }).then(restaurant => {
-      return res.render('admin/restaurant', { restaurant })
+    return Restaurant.findByPk(req.params.id, { include: [Category] }).then(restaurant => {
+      return res.render('admin/restaurant', { restaurant: restaurant.toJSON() })
     })
   },
   editRestaurant: (req, res) => {
