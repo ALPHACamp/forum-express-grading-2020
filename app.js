85ed850c143f50423a597187469ea66d97756c3b
joeyWuTRKTR <godatascial@yahoo.com>
2021-09-03 16:10:17 +0800

    feat: add clearDB test

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+var mysql = require('mysql');
+
+var db_config = {
+  host: 'us-cdbr-east-04.cleardb.com',
+  user: 'baacdf01408880',
+  password: 'ed18bc9f',
+  database: 'heroku_a094e57629ef1fa'
+};
+
+var connection;
 
 
 // 產生flash並放到session
@@ -14,12 +24,14 @@ const helpers = require('./_helpers')
 
 const app = express()
 const port = process.env.PORT || 3000
+app.use(express.logger());
 
 app.engine('handlebars', handlebars({ 
   defaultLayout: 'main', 
   helpers: require('./config/handlebars-helpers')
 }))
 app.set('view engine', 'handlebars')
+
 
 app.use(session({ secret:'secret', resave: false, saveUninitialized: false }))
 app.use(flash())
@@ -37,6 +49,40 @@ app.use((req, res, next) => {
   res.locals.user = helpers.getUser(req) // 封裝測試
   next()
 })
+
+function handleDisconnect() {
+  console.log('1. connecting to db:');
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+  // the old one cannot be reused.
+
+  connection.connect(function (err) {              	// The server is either down
+    if (err) {                                     // or restarting (takes a while sometimes).
+      console.log('2. error when connecting to db:', err);
+      setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
+    }                                     	// to avoid a hot loop, and to allow our node script to
+  });                                     	// process asynchronous requests in the meantime.
+  // If you're also serving http, display a 503 error.
+  connection.on('error', function (err) {
+    console.log('3. db error', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') { 	// Connection to the MySQL server is usually
+      handleDisconnect();                      	// lost due to either server restart, or a
+    } else {                                      	// connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
+app.get('/', function (request, response) {
+  connection.query('SELECT * from t_users', function (err, rows, fields) {
+    if (err) {
+      console.log('error: ', err);
+      throw err;
+    }
+    response.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
