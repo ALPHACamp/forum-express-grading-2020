3dd2a2c59a6125bbda95ad6f9adadf81a2ceda40
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-03 15:39:09 +0800

    feat: Show related comments on restaurant page

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,4 +1,6 @@
 const db = require('../models')
+const Comment = db.Comment
+const User = db.User
 const Restaurant = db.Restaurant
 const Category = db.Category
 const pageLimit = 10
@@ -55,11 +57,12 @@ const restController = {
     })
   },
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { include: Category }).then(
-      (restaurant) => {
-        return res.render('restaurant', { restaurant: restaurant.toJSON() })
-      }
-    )
+    return Restaurant.findByPk(req.params.id, {
+      include: [Category, { model: Comment, include: [User] }]
+    }).then((restaurant) => {
+      console.log(restaurant.Comments[0].dataValues)
+      return res.render('restaurant', { restaurant: restaurant.toJSON() })
+    })
   }
 }
 
