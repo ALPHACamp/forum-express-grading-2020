495b4418edb979fe5605d996c92211f9386c6e00
jadokao <jackykao528@outlook.com>
2021-09-21 16:08:35 +0800

    add User login system

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const db = require('./models') // 引入資料庫
 const app = express()
 const port = 3000
@@ -12,8 +13,12 @@ app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 // bodyParser
 app.use(bodyParser.urlencoded({ extended: true }))
-// session and flash
+// session
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+// passport
+app.use(passport.initialize())
+app.use(passport.session())
+// flash
 app.use(flash())
 
 app.use((req, res, next) => {
@@ -26,7 +31,7 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-// 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
-require('./routes')(app)
+// 引入 routes 並將 app, passport 傳進去
+require('./routes')(app, passport)
 
 module.exports = app
