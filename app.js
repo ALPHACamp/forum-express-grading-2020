523e5bec862856f737cdb924d3078f26ec0fc882
jadokao <jackykao528@outlook.com>
2021-09-21 15:42:48 +0800

    Add restaurant index page

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const app = express()
 const port = 3000
 
 // Handlebars 註冊樣板引擎, 設定使用 Handlebars 做為樣板引擎
-app.engine('handlebars', handlebars())
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
+// 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
 require('./routes')(app)
 
 module.exports = app
